04166779cea97afd9cd1b5798047943e97e35e3c
Ivana Plašienková <ivana.plasienkova@gmail.com>
2022-05-21 13:52:46 +0200

    zmapovanie pola cities a pridanie jeho properties do options a pridanie useEffect-vyuzitie API (zoznam cities)

--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -2,12 +2,46 @@ import React, { useEffect, useState } from 'react';
 import mapImage from './img/map.svg';
 import './style.css';
 
+const CityOptions = ({ cities }) => {
+  return (
+    <>
+      <option value="">Vyberte</option>
+      {cities.map((city) => {
+        return (
+          <option key={city.code} value={city.code}>
+            {city.name}
+          </option>
+        );
+      })}
+
+      {/* <option value="">Vyberte</option>
+      <option value="mesto01">Město 01</option>
+      <option value="mesto02">Město 02</option>
+      <option value="mesto03">Město 03</option>
+      <option value="mesto04">Město 04</option>
+      <option value="mesto05">Město 05</option> */}
+    </>
+  );
+};
+
 export const JourneyPicker = ({ onJourneyChange }) => {
   const [fromCity, setFromCity] = useState('');
   const [toCity, setToCity] = useState('');
   const [date, setDate] = useState('');
+  const [cities, setCities] = useState([]);
 
-  console.log(date);
+  useEffect(() => {
+    setCities([
+      { name: 'Praha', code: 'CZ-PRG' },
+      { name: 'Brno', code: 'CZ-BRQ' },
+    ]);
+
+    fetch(`https://apps.kodim.cz/daweb/leviexpress/api/cities`)
+      .then((response) => response.json())
+      .then((data) => setCities(data.results));
+  }, []);
+
+  console.log(cities);
   const handleSubmit = (event) => {
     event.preventDefault();
     return console.log('Odesílám formulář s cestou');
@@ -21,23 +55,13 @@ export const JourneyPicker = ({ onJourneyChange }) => {
             <div className="journey-picker__label">Odkud:</div>
             <select onChange={(e) => setFromCity(e.target.value)}>
               value={fromCity}
-              <option value="">Vyberte</option>
-              <option value="mesto01">Město 01</option>
-              <option value="mesto02">Město 02</option>
-              <option value="mesto03">Město 03</option>
-              <option value="mesto04">Město 04</option>
-              <option value="mesto05">Město 05</option>
+              <CityOptions cities={cities} />
             </select>
           </label>
           <label>
             <div className="journey-picker__label">Kam:</div>
             <select onChange={(e) => setToCity(e.target.value)} value={toCity}>
-              <option value="">Vyberte</option>
-              <option value="mesto01">Město 01</option>
-              <option value="mesto02">Město 02</option>
-              <option value="mesto03">Město 03</option>
-              <option value="mesto04">Město 04</option>
-              <option value="mesto05">Město 05</option>
+              <CityOptions cities={cities} />
             </select>
           </label>
           <label>
